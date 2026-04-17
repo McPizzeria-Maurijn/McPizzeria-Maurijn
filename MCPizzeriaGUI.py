3d5984b59dbc40e39f07b2ac7c5fcd5dbf6d1506
McPizzeria-Maurijn <114958@montessoricollege.nl>
2026-04-17 08:47:48 +0200

    zoekknop gemaakt

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -28,9 +28,21 @@ knopSluit.grid(row=17, column=4)
 labelIntro = Label(venster, text="Welkom!")
 labelIntro.grid(row=0, column=0, sticky="W") 
 
+labelKlantnaam = Label(venster, text="Klantnaam:")
+labelKlantnaam.grid(row=1, column=0)
+ 
+ingevoerde_klantnaam = StringVar() 
+invoerveldKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam) 
+invoerveldKlantnaam.grid(row=1, column=1, sticky="W") 
 
+labelKlantNr = Label(venster, text="Klantnummer:")
+labelKlantNr.grid(row=2, column=0)
 
+invoerveldKlantNr = Entry(venster) 
+invoerveldKlantNr.grid(row=2, column=1, sticky="W") 
 
+zoekKnop = Button(venster, text="Zoek klant", width=12)
+zoekKnop.grid(row=1, column=4, sticky="W")
 
 #reageert op gebruikersinvoer, deze regel als laatste laten staan
 venster.mainloop()
