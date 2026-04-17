1e714cf42b3b744aebe2b699eaa894e06bfc4036
McPizzeria-Maurijn <114958@montessoricollege.nl>
2026-04-17 09:10:49 +0200

    Klaar met alles voor Toon pizza's

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -14,42 +14,42 @@ import MCPizzeriaSQL
 
 
 ### ---------  Functie definities  -----------------
-def zoekKlant():
-    gevonden_klanten = MCPizzeriaSQL.zoekKlantInTabel(ingevoerde_klantnaam.get())
-    print(gevonden_klanten)
-    invoerveldKlantnaam.delete(0, END)
-    invoerveldKlantNr.delete(0, END)
-    for rij in gevonden_klanten:
-        invoerveldKlantNr.insert(END, rij[0])
-        invoerveldKlantnaam.insert(END, rij[1])
+#def zoekKlant():
+    #gevonden_klanten = MCPizzeriaSQL.zoekKlantInTabel(ingevoerde_klantnaam.get())
+    #print(gevonden_klanten)
+    #invoerveldKlantnaam.delete(0, END)
+    #invoerveldKlantNr.delete(0, END)
+    #for rij in gevonden_klanten:
+        #invoerveldKlantNr.insert(END, rij[0])
+        #invoerveldKlantnaam.insert(END, rij[1])
 
 ### --------- Hoofdprogramma  ---------------
 
-venster = Tk()
-venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
-venster.wm_title("MC Pizzeria")
+#venster = Tk()
+#venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
+#venster.wm_title("MC Pizzeria")
 
-knopSluit = Button(venster, text="Close", width=22, command=venster.destroy)
-knopSluit.grid(row=17, column=4)
+#knopSluit = Button(venster, text="Close", width=22, command=venster.destroy)
+#knopSluit.grid(row=17, column=4)
 
-labelIntro = Label(venster, text="Welkom!")
-labelIntro.grid(row=0, column=0, sticky="W") 
+#labelIntro = Label(venster, text="Welkom!")
+#labelIntro.grid(row=0, column=0, sticky="W") 
 
-labelKlantnaam = Label(venster, text="Klantnaam:")
-labelKlantnaam.grid(row=1, column=0)
+#labelKlantnaam = Label(venster, text="Klantnaam:")
+#labelKlantnaam.grid(row=1, column=0)
  
-ingevoerde_klantnaam = StringVar() 
-invoerveldKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam) 
-invoerveldKlantnaam.grid(row=1, column=1, sticky="W") 
+#ingevoerde_klantnaam = StringVar() 
+#invoerveldKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam) 
+#invoerveldKlantnaam.grid(row=1, column=1, sticky="W") 
 
-labelKlantNr = Label(venster, text="Klantnummer:")
-labelKlantNr.grid(row=2, column=0)
+#labelKlantNr = Label(venster, text="Klantnummer:")
+#labelKlantNr.grid(row=2, column=0)
 
-invoerveldKlantNr = Entry(venster) 
-invoerveldKlantNr.grid(row=2, column=1, sticky="W") 
+#invoerveldKlantNr = Entry(venster) 
+#invoerveldKlantNr.grid(row=2, column=1, sticky="W") 
 
-zoekKnop = Button(venster, text="Zoek klant", width=12, command=zoekKlant)
-zoekKnop.grid(row=1, column=4, sticky="W")
+#zoekKnop = Button(venster, text="Zoek klant", width=12, command=zoekKlant)
+#zoekKnop.grid(row=1, column=4, sticky="W")
 
 #reageert op gebruikersinvoer, deze regel als laatste laten staan
 venster.mainloop()
